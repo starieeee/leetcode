fd0d6b8c74d50478d91020d58ccc027ea59f33c3
star <natepy19@gmail.com>
2025-10-24 13:23:49 -0500

    commit

---
+++ easy/mergeTwoSortedList.py
@@ -0,0 +1,13 @@
+def mergeSortedTwoLists(list1, list2):
+    dummy = ListNode()
+    current = dummy
+    while list1 and list2:
+        if list1.val < list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
+    current.next = list1 or list2
+    return dummy.next
