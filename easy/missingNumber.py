60fb7e89d043b0e40a6a13a07fd504aa96de3633
Natepy Vann <162285803+starieeee@users.noreply.github.com>
2025-11-15 15:03:47 -0600

    commit

---
+++ easy/missingNumber.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(nums)
+        for i in range(len(nums)):
+            if nums[i] != i:
+                return i
+        return len(nums)
+
