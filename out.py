132d86f86a403902eed4dcf9d1ad919bb80879c3
Natepy Vann <162285803+starieeee@users.noreply.github.com>
2025-11-19 19:53:07 -0600

    commit

---
+++ out.py
@@ -0,0 +1,9 @@
+def out():
+    phrase = input("Enter phrase: ")
+    time = int(input("Enter number times: "))
+    for i in range(time):
+                print(phrase.upper())
+               
+def main():
+    out()
+main()
